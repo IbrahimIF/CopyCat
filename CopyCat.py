9392e68523dc86d55ceb7a29c458fe68f612c531
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-05-11 16:03:34 +0100

    added recordAudio and micToText Functions
    Changes:
    - Added a function that records your microphone audio using pyaudio
    - Added a funciton that takes the audio saved within a .wav file and converts the speech into text.
    
    - the current textToSpeech elevenlabs function now takes the variable text from micToText().

--- CopyCat.py
+++ CopyCat.py
@@ -1,15 +1,93 @@
+import pyaudio
+import wave
+import time
+
+import speech_recognition as sr
+
 from elevenlabs import save
 from elevenlabs.client import ElevenLabs
 from playsound import playsound
 
-client = ElevenLabs(
-  api_key="sk_d9959509b80a843fcf8baf8fcf1c691da818dadec9274c47", # Defaults to ELEVEN_API_KEY
-)
 
-audio = client.generate(
-  text="This is the beginning of the Copycat Project, where I repeat whatever you say.",
-  voice="Daniel",
-)
+def recordAudio():
+  FORMAT = pyaudio.paInt16
+  CHANNELS = 1
+  RATE = 44100
+  CHUNK = 1024
+  OUTPUT_FILENAME = "recordFile.wav"
 
-save(audio, "output.mp3")
-playsound("output.mp3")
+  audio = pyaudio.PyAudio()
+  stream = audio.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK
+  )
+
+  print("recording start")
+  frames = []
+  seconds = 5
+
+  for i in range(0, int(RATE / CHUNK * seconds)):    
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+  print("recording stopped")
+  
+  stream.stop_stream()
+  stream.close()
+  audio.terminate()
+  
+  wf = wave.open(OUTPUT_FILENAME, 'wb')
+  wf.setnchannels(CHANNELS)
+  wf.setsampwidth(audio.get_sample_size(FORMAT))
+  wf.setframerate(RATE)
+  wf.writeframes(b''.join(frames))
+  wf.close()
+
+
+
+def micToText():
+  """Process the recorded audio"""
+  print("Processing audio...")
+  try:
+      with sr.AudioFile('recordFile.wav') as source:
+            r = sr.Recognizer()
+            r.energy_threshold = 5000
+            r.pause_threshold = 2.0
+                
+            print("Listening...")
+            audio = r.listen(source)
+                
+            print("Processing audio...")
+            
+            said = r.recognize_google(audio)
+            print("Full transcript:", said)
+            return said.lower()
+  except Exception as e:
+      print(e)
+      print("Google didn't understand.")
+      return "none"
+
+
+def textToSpeech():
+  print("test")
+  text = micToText()
+  client = ElevenLabs(
+    api_key="sk_d9959509b80a843fcf8baf8fcf1c691da818dadec9274c47", # Defaults to ELEVEN_API_KEY
+  )
+  
+  audio = client.generate(
+    text=text,
+    voice="Brian",
+  )
+  
+  save(audio, "output.mp3")
+  playsound("output.mp3")
+
+
+
+
+recordAudio()
+textToSpeech()
